0b1524987cbdad59ece1525867e0b1da74eab4eb
albertmoralees <albertmoralesmora@gmail.com>
2025-07-15 18:54:26 -0600

    feat: add Header and Footer components, wrap pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
 import HomePage from './pages/HomePage'
 import ServicesPage from './pages/ServicesPage'
 import BlogPage from './pages/BlogPage'
@@ -7,12 +9,14 @@ import ContactPage from './pages/ContactPage'
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/services" element={<ServicesPage />} />
         <Route path="/blog" element={<BlogPage />} />
         <Route path="/contact" element={<ContactPage />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
